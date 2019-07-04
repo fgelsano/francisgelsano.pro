f48dce5535b73d549ee11a605adee96b9d9a1ae8
Francis Gelsano <fgelsano.virus@gmail.com>
2019-07-04 10:59:59 +0800

    Added magnify on hover

--- resources/views/partials/front/_tech-stack.blade.php
+++ resources/views/partials/front/_tech-stack.blade.php
@@ -2,37 +2,37 @@
     <h2>My Tech Stack</h2>  
     <div class="row mb-5">
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="50" data-toggle="tooltip" data-placement="top" title="Confidence Level: 9 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/html.png') }}" alt="HTML" width="50%" title="HTML">
+            <img src="{{ asset('storage/local_assets/skills/html.png') }}" alt="HTML" width="50%" title="HTML" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="100" data-toggle="tooltip" data-placement="top" title="Confidence Level: 8 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/css.png') }}" alt="CSS" width="50%"  title="CSS">
+            <img src="{{ asset('storage/local_assets/skills/css.png') }}" alt="CSS" width="50%"  title="CSS" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="150" data-toggle="tooltip" data-placement="top" title="Confidence Level: 8 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/bootstrap.png') }}" alt="Bootstrap" width="50%" title="Bootstrap">
+            <img src="{{ asset('storage/local_assets/skills/bootstrap.png') }}" alt="Bootstrap" width="50%" title="Bootstrap" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="200" data-toggle="tooltip" data-placement="top" title="Confidence Level: 7 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/php.png') }}" alt="PHP" width="50%" title="PHP">
+            <img src="{{ asset('storage/local_assets/skills/php.png') }}" alt="PHP" width="50%" title="PHP" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="250" data-toggle="tooltip" data-placement="top" title="Confidence Level: 6 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/mysql.png') }}" alt="MySQL" width="50%" title="MySQL">
+            <img src="{{ asset('storage/local_assets/skills/mysql.png') }}" alt="MySQL" width="50%" title="MySQL" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="300" data-toggle="tooltip" data-placement="top" title="Confidence Level: 7 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/wordpress.png') }}" alt="WordPress" width="50%" title="WordPress">
+            <img src="{{ asset('storage/local_assets/skills/wordpress.png') }}" alt="WordPress" width="50%" title="WordPress" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="350" data-toggle="tooltip" data-placement="top" title="Confidence Level: 6 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/git.png') }}" alt="WordPress" width="50%" title="Git">
+            <img src="{{ asset('storage/local_assets/skills/git.png') }}" alt="WordPress" width="50%" title="Git" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="400" data-toggle="tooltip" data-placement="top" title="Confidence Level: 6 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/github.png') }}" alt="WordPress" width="50%" title="GitHub">
+            <img src="{{ asset('storage/local_assets/skills/github.png') }}" alt="WordPress" width="50%" title="GitHub" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="450" data-toggle="tooltip" data-placement="top" title="Confidence Level: 6 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/laravel.png') }}" alt="WordPress" width="50%" title="Laravel">
+            <img src="{{ asset('storage/local_assets/skills/laravel.png') }}" alt="WordPress" width="50%" title="Laravel" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="500" data-toggle="tooltip" data-placement="top" title="Confidence Level: 6 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/laradock.png') }}" alt="WordPress" width="50%" title="Laradock">
+            <img src="{{ asset('storage/local_assets/skills/laradock.png') }}" alt="WordPress" width="50%" title="Laradock" class="skill">
         </div>
         <div class="col-12 col-sm-3 col-md-3 col-lg-2 text-center mt-5" data-aos="zoom-in" data-aos-offset="550" data-toggle="tooltip" data-placement="top" title="Confidence Level: 5 out of 10">
-            <img src="{{ asset('storage/local_assets/skills/docker.png') }}" alt="WordPress" width="50%" title="Docker">
+            <img src="{{ asset('storage/local_assets/skills/docker.png') }}" alt="WordPress" width="50%" title="Docker" class="skill">
         </div>
     </div>
 </div>
